3d9426cdcea3e73c326ce9b0a889d6f5687a68d3
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-04 22:14:32 +0530

    Router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-row justify-center items-center bg-slate-900 text-cyan-50">
+    <div className="h-screen flex flex-row justify-center items-center bg-slate-900 text-cyan-50 text-3xl">
       App
     </div>
   )
