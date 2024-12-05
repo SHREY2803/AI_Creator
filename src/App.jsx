c295e0fac566d5808619e4bdad87fa8118c1c5a0
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-05 22:10:41 +0530

    Button Component made.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
+
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-row justify-center items-center bg-slate-900 text-cyan-50 text-3xl">
-      App
-    </div>
-  )
-}
+    <>
+      {/* <div className="h-screen flex flex-row justify-center items-center bg-slate-900 text-cyan-50 text-3xl">
+        App
+      </div> */}
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">Something</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
 
-export default App
+export default App;
