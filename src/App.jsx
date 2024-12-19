015727dc052648a4c645fdf028b100e396132082
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-19 22:21:41 +0530

    Header comp name changed to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
+        <Navbar />
       </div>
       <ButtonGradient />
     </>
