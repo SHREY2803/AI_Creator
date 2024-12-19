1b0a6852ee9daae5681ca36e8d33e890cb1bc28c
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-19 16:47:46 +0530

    Changes made in Button component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 
-
-
 const App = () => {
   return (
     <>
-      {/* <div className="h-screen flex flex-row justify-center items-center bg-slate-900 text-cyan-50 text-3xl">
+      {/* <div className="flex flex-row items-center justify-center h-screen text-3xl bg-slate-900 text-cyan-50">
         App
       </div> */}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">Something</Button>
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
       </div>
       <ButtonGradient />
     </>
