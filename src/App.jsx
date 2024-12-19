974daa30f6474d08f9ab969758563aa35a032a7a
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-19 17:32:49 +0530

    Header component pushed in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      {/* <div className="flex flex-row items-center justify-center h-screen text-3xl bg-slate-900 text-cyan-50">
-        App
-      </div> */}
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
