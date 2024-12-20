ee6ddfb3190379b21bbd9da24f5f39e4eb39aa9a
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-20 11:56:59 +0530

    Added togglenav fn for changing the value of our state from true to false and vice versa, handleclick for each item in the navv in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,37 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "../design/Header";
 
-
 const Navbar = () => {
   const pathnName = useLocation(); //this will give us access to the current url we are on.
-  const [openNavigation, setOpenNavigation] = useState(false);
+  const [openNav, setOpenNav] = useState(false);
+  const toggleNav = () =>{
+    if(openNav){
+      setOpenNav(false);
+    }
+    else{
+      setOpenNav(true);
+    } 
+  };
+  const handleClick = () => {
+    setOpenNav(false);
+  }
   return (
-    <div className="fixed top-0 left-0 z-50 w-full border-b bg-n-8/90 backdrop-blur-sm border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-      {" "}
+    <div
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNav ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       {/*Use the backdrop-blur-* utilities to control an element’s backdrop blur. */}
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brainwave-img" />
         </a>
 
-        <nav className="fixed hidden top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+          className={`${
+            openNav ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
           <div className="relative flex flex-col items-center justify-center m-auto z-2 lg:flex-row">
             {navigation.map((item) => (
               <a
@@ -32,6 +49,7 @@ const Navbar = () => {
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                onClick={handleClick}
               >
                 {" "}
                 {/*so here basically we used the useLocation to get which page we are on and based on that if the url matched with the page we are on we basically made that page title white other than other pages. */}
@@ -39,6 +57,7 @@ const Navbar = () => {
               </a>
             ))}
           </div>
+          <HamburgerMenu />
         </nav>
         <a
           href="#signup"
@@ -46,7 +65,12 @@ const Navbar = () => {
         >
           New Account
         </a>
-        <Button className="hidden lg:flex" href="#login">LogIn</Button>
+        <Button className="ml-auto lg:hidden" px="px-5" onClick={toggleNav}>
+          <MenuSvg openNavigation={openNav} />
+        </Button>
+        <Button className="hidden lg:flex" href="#login">
+          LogIn
+        </Button>
       </div>
     </div>
   );
