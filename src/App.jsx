5ef32eebd8756dc1ae69a3ed067d27c98b23de91
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-20 09:58:57 +0530

    understanding the button comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import ButtonGradient from "./assets/svg/ButtonGradient";
+import ButtonGradient from "./assets/svg/ButtonGradient"; //this buttongradient is not a svg file, designed to define reusable gradient styles that can be applied to various elements, such as buttons, text, and backgrounds.
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+    
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Navbar />
+        {/* <Button className="mt-10" href="#login">Something</Button> */}
       </div>
       <ButtonGradient />
     </>
