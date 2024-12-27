f7019f5953822c0939fcb97c75df3744243fd06e
Shrey Kulkarni <shrey280302@gmail.com>
2024-12-27 23:09:33 +0530

    Hero section added in the app section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient"; //this buttongradient is not a svg file, designed to define reusable gradient styles that can be applied to various elements, such as buttons, text, and backgrounds.
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
+import Hero from "./design/Hero";
+
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Navbar />
         {/* <Button className="mt-10" href="#login">Something</Button> */}
+        <Hero />
+        
       </div>
       <ButtonGradient />
     </>
