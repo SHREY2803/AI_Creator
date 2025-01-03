214ca973b8fea81d8667f8842b16d913a92e110f
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-03 14:40:18 +0530

    Photo and text added.

--- src/design/Notification.jsx
+++ src/design/Notification.jsx
@@ -9,23 +9,35 @@ const Notification = ({ className, title }) => {
         className || ""
       } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
     >
+      <img 
+      src={notification1} 
+      alt="notification1"
+      width={62}
+      height={62}
+      className="rounded-xl"
+      />
       <div className="flex-1">
         <h6 className="mb-1 text-base font-semibold ">{title}</h6>
-      </div>
 
-      <div className="flex items-center justify-between">
-        <ul className="flex gap-2 -m-0.5">
-          {notificationImages.map((image, index) => (
-            <li key={index} className="flex w-6 h-6 overflow-hidden border-2 rounded-full border-n-12 " >
-              <img 
-              src={image}
-              className= "w-full"
-              alt={image} 
-              width={20} 
-              height={20} />
-            </li>
-          ))}
-        </ul>
+        <div className="flex items-center justify-between">
+          <ul className="flex -m-0.5">
+            {notificationImages.map((image, index) => (
+              <li
+                key={index}
+                className="flex w-6 h-6 overflow-hidden border-2 rounded-full border-n-12 "
+              >
+                <img
+                  src={image}
+                  className="w-full"
+                  alt={image}
+                  width={20}
+                  height={20}
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="bod-2 text-n-13">3min ago</div>
+        </div>
       </div>
     </div>
   );
