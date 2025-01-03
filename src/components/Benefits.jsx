9f225c42faad9eed9332232a2dab2a294c2387c9
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-03 16:40:52 +0530

    Benefit section further hero section made.

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Section from "../design/Section";
+import Heading from "../design/Heading";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading className="" title="Chat Smarter, Not Harder With BrainWave" />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
