912c0f8e1c12d7545490c371d4a73ed98e8f04c6
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-06 16:41:16 +0530

    Pricing component.

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section from "../design/Section";
+
+const Pricing = () => {
+  return <Section className="overflow-hidden" crosses id="pricing">
+    <div className="container relative z-2">
+        <div className="relative justify-center hidden mb-"></div>
+    </div>
+  </Section>;
+};
+
+export default Pricing;
