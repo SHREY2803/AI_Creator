b4cc5c4cf4360a81feccc9e571ae13a64f2736e2
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-06 10:52:58 +0530

    Made another card of ai image.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,6 +7,7 @@ import {
   brainwaveServicesIcons,
 } from "../constants/index.js";
 import Generating from "../design/Generating.jsx";
+import {PhotoChatMessage, Gradient, VideoBar, VideoChatMessage} from "../design/Services.jsx";
 
 const Services = () => {
   return (
@@ -45,11 +46,28 @@ const Services = () => {
               </ul>
             </div>
 
-            <Generating className="absolute border lg:-translate-x-1/2 left-4 right-4 bottom-4 border-n-1/10 lg:left-1/2 lg-right-auto lg:bottom-8"/>
+            <Generating className="absolute border lg:-translate-x-1/2 left-4 right-4 bottom-4 border-n-1/10 lg:left-1/2 lg-right-auto lg:bottom-8" />
           </div>
-          
-          <div className="">
-            
+
+          <div className="grid gap-5 realtive z-1 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={service2}
+                  alt="secondRobo"
+                  className="object-cover w-full h-full"
+                  width={630}
+                  height={750}
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-16">
+                <h4 className="mb-4 h4">Photo Editing</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Automatically ehnance your photos using AI's app's photo edit feature to make your photos look professional.
+                </p>
+              </div>
+              <PhotoChatMessage />
+            </div>
           </div>
         </div>
       </div>
