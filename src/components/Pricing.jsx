1dc030f015b1583c21d7735bd66bb729106c63b2
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-06 17:07:16 +0530

    This is pricing component.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Section from "../design/Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "../design/Heading.jsx";
 
 const Pricing = () => {
-  return <Section className="overflow-hidden" crosses id="pricing">
-    <div className="container relative z-2">
-        <div className="relative justify-center hidden mb-"></div>
-    </div>
-  </Section>;
+  return (
+    <Section className="overflow-hidden" crosses id="pricing">
+      <div className="container relative z-2">
+        <div className="relative justify-center hidden mb-[6.5rem] lg:flex">
+          <img
+            className="relative z-1"
+            src={smallSphere}
+            width={255}
+            height={255}
+            alt="Sphere"
+          />
+          <div className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[60rem] pointer-events-none">
+            <img
+              className="w-full"
+              src={stars}
+              width={950}
+              height={400}
+              alt="star"
+            />
+          </div>
+        </div>
+        <Heading
+        tag="Get Started with BrainWave"
+        title="Pay once, use forever"/>
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
