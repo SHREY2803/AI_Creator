c82c95aebca1ef5ad14a98b7c7d6a07a5c130c7f
Shrey Kulkarni <shrey280302@gmail.com>
2025-01-06 17:06:37 +0530

    in this we have add tagline component and tag prop.

--- src/design/Heading.jsx
+++ src/design/Heading.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import TagLine from './Tagline.jsx';
 
-const Heading = ({className, title, text}) => {
+const Heading = ({className, title, text, tag}) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20  `}>
+      {tag && (
+        <TagLine className={`mb-4 justify-center`}>{tag}</TagLine>
+      )}
       {title && <h2 className='h2'>{title}</h2>}
       {text && <p className='mt-4 body-2 text-n-4'>{text}</p>}
     </div>
